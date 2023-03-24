f9d48acbe33d0fc66cd9c9195586460e968f8632
Martin Zheng <jszhengyq@gmail.com>
2023-03-24 14:32:41 -0700

    feat(lib)!: added sources of dynamic settings for babel,eslint,typescript,jest

--- bin/mole-build.js
+++ bin/mole-build.js
@@ -2,9 +2,12 @@
 
 const { execSync } = require('child_process');
 
-execSync([
-  'rm -rf dist',
-  'npx @pandazy/path-alias',
-  'yarn tsc --build tsconfig-build.json',
-  'yarn babel src --out-dir dist --extensions ".ts,.tsx"',
-].join(' && '), { stdio: 'inherit' });
+execSync(
+  [
+    'rm -rf dist',
+    'npx @pandazy/path-alias',
+    'yarn tsc --build tsconfig-build.json',
+    'yarn babel src --out-dir dist --extensions ".ts,.tsx"',
+  ].join(' && '),
+  { stdio: 'inherit' }
+);
