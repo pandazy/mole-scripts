04df2e96a9fd8fac9d72b75f1895b2e85f0efa5d
Martin Zheng <jszhengyq@gmail.com>
2023-03-29 17:46:50 -0700

    fix(path-alias): updated path alias dependency, removed the redundant use of path-alias command

--- bin/mole-build.js
+++ bin/mole-build.js
@@ -5,7 +5,6 @@ const { execSync } = require('child_process');
 execSync(
   [
     'rm -rf dist',
-    'npx @pandazy/path-alias',
     'yarn tsc --build tsconfig-build.json',
     'yarn babel src --out-dir dist --extensions ".ts,.tsx"',
   ].join(' && '),
